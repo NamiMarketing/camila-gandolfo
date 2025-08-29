0adab1313f33edd9e8b7994119c60531441da365
Marcelo Cho <marcelo.cho@gmail.com>
2025-08-29 18:16:54 +0900

    mobile home

--- .eleventy.js
+++ .eleventy.js
@@ -62,6 +62,20 @@ module.exports = function (eleventyConfig) {
     },
   );
 
+  
+  eleventyConfig.setServerOptions({
+    middleware: [
+        function (req, res, next) {
+            if (req.statusCode === 404) {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(require("fs").readFileSync("_site/404.html"));
+            } else {
+                next();
+            }
+        }
+    ]
+});
+
   return {
     dir: {
       input: "src",
